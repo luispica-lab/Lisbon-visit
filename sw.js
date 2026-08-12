3c07733c85adf96925de63e48fe760f7a4e781ad
luispica-lab <luispica@gmail.com>
2026-08-12 11:23:30 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jtj-lisbon-v10';
+const CACHE = 'jtj-lisbon-v12';
 const ASSETS = ['./', './index.html', './app.dat', './manifest.webmanifest',
                 './icon-192.png', './icon-512.png', './icon-180.png'];
 
